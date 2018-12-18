e5cdfd3dd5e0f3edd6f318682df96306ad60eb82
Plesz Gábor <gabor.plesz@gmail.com>
2018-12-18 11:40:28 +0100

    #2: az első teszt forgatókönyv vázlat

---
+++ pg.DarkSky.api.tests/ApiRequests.feature
@@ -0,0 +1,10 @@
+﻿Jellemző: ApiRequests
+	Fejlesztőként szükségem van egy osztályra, amin keresztül elérem az API felületet.
+
+Forgatókönyv vázlat: GetDataWithCoordinateAndLanguage
+	Adott egy API kapcsolat '<ApiKulcs>' '<Város>' és '<Nyelv>' adatokkal
+	Ha lekérdezem az időjárási adatokat
+	Akkor a válasz <Érvényes> lesz
+Példák: 
+| tesztnév | ApiKulcs                         | Város             | Nyelv | Érvényes |
+| Bp-hu    | 2f4659626fb968a85a5ff22561962711 | 47.49801,19.03991 | hu    | true     |
---
+++ pg.DarkSky.api.tests/ApiRequestsSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace pg.DarkSky.api.tests
+{
+    [Binding]
+    public class ApiRequestsSteps
+    {
+        [Given(@"egy API kapcsolat '(.*)' '(.*)' és '(.*)' adatokkal")]
+        public void AmennyibenEgyAPIKapcsolatEsAdatokkal(string apikey, string coordinates, string lang)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"lekérdezem az időjárási adatokat")]
+        public void MajdLekerdezemAzIdojarasiAdatokat()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"a válasz true lesz")]
+        public void AkkorAValaszTrueLesz()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
