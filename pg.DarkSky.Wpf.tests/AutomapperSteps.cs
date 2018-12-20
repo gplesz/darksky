2471dab8d646b1c9acd40f56c3077042df002f0d
Plesz Gábor <gabor.plesz@gmail.com>
2018-12-20 17:10:43 +0100

    #9: model első prototípus kész

--- pg.DarkSky.Wpf.tests/AutomapperSteps.cs
+++ pg.DarkSky.Wpf.tests/AutomapperSteps.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pg.DarkSky.api.Model;
 using pg.DarkSky.api.Model.FromDarkSky;
 using pg.DarkSky.Wpf.Models;
 using pg.DarkSky.Wpf.Profiles;
@@ -19,6 +20,7 @@ namespace pg.DarkSky.Wpf.tests
         private MapperConfiguration config;
         private ForecastModelDataPoint forecast;
         private List<ForecastModelDataPoint> forecastList;
+        private ForecastModel apiResult;
 
         public AutomapperSteps()
         {
@@ -45,7 +47,8 @@ namespace pg.DarkSky.Wpf.tests
         [When(@"ellenőrzöm a konfigurációt")]
         public void MajdEllenorzomAKonfiguraciot()
         {
-            //ezzel most nincs dolgunk
+            //ezzel most nincs dolgunk, mert nincs köztes lépés, csak a jellemző (AutomapperFeature)
+            //forgatókönyve (Automapper configuration check) miatt kell egy ilyen hívás váza
         }
 
         [Then(@"a végeredmény azt jelzi, hogy minden rendben")]
@@ -59,8 +62,97 @@ namespace pg.DarkSky.Wpf.tests
         [When(@"egy CurrentDataPoint példányt transzformálok ForecastModelDataPoint típusra")]
         public void MajdEgyCurrentDataPointPeldanytTranszformalokForecastModelDataPointTipusra()
         {
+            var current = CurrentlyDataPointFactory();
+
+            forecast = mapper.Map<ForecastModelDataPoint>(current);
+
+            logger.Information("Mapped from {@Current} to {@Forecast}", current, forecast);
+
+        }
+
+        [When(@"egy DailyDataPoint példányt transzformálok ForecastModelDataPoint típusra")]
+        public void MajdEgyDailyDataPointPeldanytTranszformalokForecastModelDataPointTipusra()
+        {
             //todo: itt lehetne a minimumra és még egy köztes értékre tesztet írni
-            var current = new CurrentlyDataPoint
+            var daily = DailyDataPointFactory();
+
+            forecast = mapper.Map<ForecastModelDataPoint>(daily);
+
+            logger.Information("Mapped from {@Daily} to {@Forecast}", daily, forecast);
+        }
+
+        [Then(@"a ForecastDataPoint példány jól van kitöltve")]
+        public void AkkorAForecastDataPointPeldanyJolVanKitoltve()
+        {
+            AssertForecastDataPoint(forecast);
+        }
+
+        [When(@"egy ListOfDailyDataPoint példányt transzformálok ListOfForecastModelDataPoint típusra")]
+        public void MajdEgyListOfDailyDataPointPeldanytTranszformalokListOfForecastModelDataPointTipusra()
+        {
+            List<DailyDataPoint> dailyList = DailyDataPointListFactory();
+
+            forecastList = mapper.Map<List<ForecastModelDataPoint>>(dailyList);
+
+            logger.Information("Mapped from {@DailyList} to {@ForecastList}", dailyList, forecastList);
+        }
+
+        [Then(@"a ListOfForecastModelDataPoint példány jól van kitöltve")]
+        public void AkkorAListOfForecastModelDataPointPeldanyJolVanKitoltve()
+        {
+            AssertListOfForecastDataPointList(forecastList);
+        }
+
+        [When(@"egy ApiResult példányt transzformálok ForecastModel típusra")]
+        public void MajdEgyApiResultPeldanytTranszformalokForecastModelTipusra()
+        {
+            var result = new ApiResult<ApiResponse> {
+                ForecastApiCalls = 11,
+                HasSuccess = true,
+                Data = new ApiResponse
+                {
+                    currently = CurrentlyDataPointFactory(),
+                    daily = new DailyDataBlock
+                    {
+                        data = DailyDataPointListFactory(),
+                        summary = "bumm bumm",
+                        icon = "bamm bamm"
+                    }
+                }
+            };
+
+            apiResult = mapper.Map<ForecastModel>(result);
+        }
+
+        [Then(@"a ForecastModel példány jól van kitöltve")]
+        public void AkkorAForecastModelPeldanyJolVanKitoltve()
+        {
+            Assert.AreEqual(11, apiResult.ForecastApiCalls);
+            Assert.IsTrue(apiResult.IsValid);
+            AssertForecastDataPoint(apiResult.Current);
+            AssertListOfForecastDataPointList(apiResult.Daily);
+        }
+
+        private static List<DailyDataPoint> DailyDataPointListFactory()
+        {
+            //todo: itt lehetne a minimumra és még egy köztes értékre tesztet írni
+
+            //készítünk egy listát amit mappelünk, hogy teszteljük a heti előrejelzés mappingjét
+            var dailyList = new List<DailyDataPoint>();
+
+            for (int i = 0; i < 7; i++)
+            {
+                var daily = DailyDataPointFactory();
+                dailyList.Add(daily);
+            }
+
+            return dailyList;
+        }
+
+        private static CurrentlyDataPoint CurrentlyDataPointFactory()
+        {
+            //todo: itt lehetne a minimumra és még egy köztes értékre tesztet írni
+            return new CurrentlyDataPoint
             {
                 time = 1545087600,
                 summary = "Ködös idő reggel.",
@@ -80,22 +172,6 @@ namespace pg.DarkSky.Wpf.tests
                 visibility = double.MaxValue,
                 ozone = double.MaxValue
             };
-
-            forecast = mapper.Map<ForecastModelDataPoint>(current);
-
-            logger.Information("Mapped from {@Current} to {@Forecast}", current, forecast);
-
-        }
-
-        [When(@"egy DailyDataPoint példányt transzformálok ForecastModelDataPoint típusra")]
-        public void MajdEgyDailyDataPointPeldanytTranszformalokForecastModelDataPointTipusra()
-        {
-            //todo: itt lehetne a minimumra és még egy köztes értékre tesztet írni
-            var daily = DailyDataPointFactory();
-
-            forecast = mapper.Map<ForecastModelDataPoint>(daily);
-
-            logger.Information("Mapped from {@Daily} to {@Forecast}", daily, forecast);
         }
 
         private static DailyDataPoint DailyDataPointFactory()
@@ -107,8 +183,6 @@ namespace pg.DarkSky.Wpf.tests
                 icon = "fog",
                 precipIntensity = double.MaxValue,
                 precipProbability = double.MaxValue,
-                //temperature = double.MaxValue,
-                //apparentTemperature = double.MaxValue,
                 dewPoint = double.MaxValue,
                 humidity = double.MaxValue,
                 pressure = double.MaxValue,
@@ -118,17 +192,42 @@ namespace pg.DarkSky.Wpf.tests
                 cloudCover = double.MaxValue,
                 uvIndex = int.MaxValue,
                 visibility = double.MaxValue,
-                ozone = double.MaxValue
+                ozone = double.MaxValue,
+
+                sunriseTime = 1545087600,
+                sunsetTime = 1545087600,
+                windGustTime = 1545087600,
+                uvIndexTime = 1545087600,
+
+                moonPhase = double.MaxValue,
+                precipIntensityMax = double.MaxValue,
+                precipIntensityMaxTime = 1545087600,
+                precipAccumulation = double.MaxValue,
+                precipType = "DuDuDu",
+                temperatureHigh = double.MaxValue,
+                temperatureHighTime = 1545087600,
+                temperatureLow = double.MaxValue,
+                temperatureLowTime = 1545087600,
+                apparentTemperatureHigh = double.MaxValue,
+                apparentTemperatureHighTime = 1545087600,
+                apparentTemperatureLow = double.MaxValue,
+                apparentTemperatureLowTime = 1545087600,
+                temperatureMin = double.MaxValue,
+                temperatureMinTime = 1545087600,
+                temperatureMax = double.MaxValue,
+                temperatureMaxTime = 1545087600,
+                apparentTemperatureMin = double.MaxValue,
+                apparentTemperatureMinTime = 1545087600,
+                apparentTemperatureMax = double.MaxValue,
+                apparentTemperatureMaxTime = 1545087600,
+
             };
         }
 
-        [Then(@"a ForecastDataPoint példány jól van kitöltve")]
-        public void AkkorAForecastDataPointPeldanyJolVanKitoltve()
+        private static void AssertForecastDataPoint(ForecastModelDataPoint sut)
         {
-            var sut = forecast;
-
             Assert.IsNotNull(sut);
-            Assert.AreEqual(DateTime.Parse(""), sut.Time);
+            Assert.AreEqual(new DateTimeOffset(2018, 12, 17, 23, 0, 0, TimeSpan.FromSeconds(0)), sut.Time);
             Assert.AreEqual("Ködös idő reggel.", sut.Summary);
             Assert.AreEqual("fog", sut.Icon);
             Assert.AreEqual(double.MaxValue, sut.Temperature);
@@ -139,58 +238,14 @@ namespace pg.DarkSky.Wpf.tests
             Assert.AreEqual(int.MaxValue, sut.UvIndex);
         }
 
-        [When(@"egy ListOfDailyDataPoint példányt transzformálok ListOfForecastModelDataPoint típusra")]
-        public void MajdEgyListOfDailyDataPointPeldanytTranszformalokListOfForecastModelDataPointTipusra()
+        private static void AssertListOfForecastDataPointList(List<ForecastModelDataPoint> sut)
         {
-            //todo: itt lehetne a minimumra és még egy köztes értékre tesztet írni
-
-            //készítünk egy listát amit mappelünk, hogy teszteljük a heti előrejelzés mappingjét
-            var dailyList = new List<DailyDataPoint>();
-
-            for (int i = 0; i < 7; i++)
-            {
-                var daily = DailyDataPointFactory();
-                dailyList.Add(daily);
-            }
-
-            forecastList = mapper.Map<List<ForecastModelDataPoint>>(dailyList);
-
-            logger.Information("Mapped from {@DailyList} to {@ForecastList}", dailyList, forecastList);
-        }
-
-        [Then(@"a ListOfForecastModelDataPoint példány jól van kitöltve")]
-        public void AkkorAListOfForecastModelDataPointPeldanyJolVanKitoltve()
-        {
-            var sut = forecastList;
-
             Assert.IsNotNull(sut);
-            Assert.AreEqual(8, sut.Count);
+            Assert.AreEqual(7, sut.Count);
             foreach (var fsut in sut)
             {
-                Assert.IsNotNull(fsut);
-                Assert.AreEqual(DateTime.Parse(""), fsut.Time);
-                Assert.AreEqual("Ködös idő reggel.", fsut.Summary);
-                Assert.AreEqual("fog", fsut.Icon);
-                Assert.AreEqual(double.MaxValue, fsut.Temperature);
-                Assert.AreEqual(double.MaxValue, fsut.ApparentTemperature);
-                Assert.AreEqual(double.MaxValue, fsut.AtmosphericPressure);
-                Assert.AreEqual(double.MaxValue, fsut.WindSpeed);
-                Assert.AreEqual(double.MaxValue, fsut.Humidity);
-                Assert.AreEqual(int.MaxValue, fsut.UvIndex);
+                AssertForecastDataPoint(fsut);
             }
         }
-
-        [When(@"egy ApiResult példányt transzformálok ForecastModel típusra")]
-        public void MajdEgyApiResultPeldanytTranszformalokForecastModelTipusra()
-        {
-            ScenarioContext.Current.Pending();
-        }
-
-        [Then(@"a ForecastModel példány jól van kitöltve")]
-        public void AkkorAForecastModelPeldanyJolVanKitoltve()
-        {
-            ScenarioContext.Current.Pending();
-        }
-
     }
 }
--- pg.DarkSky.Wpf/Profiles/ForecastProfile.cs
+++ pg.DarkSky.Wpf/Profiles/ForecastProfile.cs
@@ -2,10 +2,6 @@
 using pg.DarkSky.api.Model.FromDarkSky;
 using pg.DarkSky.Wpf.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace pg.DarkSky.Wpf.Profiles
 {
@@ -13,14 +9,30 @@ namespace pg.DarkSky.Wpf.Profiles
     {
         public ForecastProfile()
         {
-            CreateMap<CurrentlyDataPoint, ForecastModelDataPoint>();
+            CreateMap<CurrentlyDataPoint, ForecastModelDataPoint>()
+                //todo: az időzónát még használni kéne
+                .ForMember(d => d.Time, o => o.MapFrom(s => FromUnixTimeToDateTime(s.time)))
+                //ez jó így?
+                .ForMember(d => d.AtmosphericPressure, o => o.MapFrom(s =>s.pressure))
+                ;
 
-            CreateMap<DailyDataPoint, ForecastModelDataPoint>();
+            CreateMap<DailyDataPoint, ForecastModelDataPoint>()
+                .ForMember(d => d.Time, o => o.MapFrom(s => FromUnixTimeToDateTime(s.time)))
+                //todo: ezt még kidolgozni
+                .ForMember(d => d.Temperature, o => o.MapFrom(s => (s.temperatureHigh / 2 + s.temperatureLow / 2)))
+                .ForMember(d => d.ApparentTemperature, o => o.MapFrom(s => (s.apparentTemperatureLow / 2 + s.apparentTemperatureHigh / 2)))
+                .ForMember(d => d.AtmosphericPressure, o => o.MapFrom(s => s.pressure))
+                ;
 
             CreateMap<api.Model.ApiResult<ApiResponse>, ForecastModel>()
                 .ForMember(d => d.IsValid, o => o.MapFrom(s => s.HasSuccess))
                 .ForMember(d => d.Current, o => o.MapFrom(s => s.Data.currently))
                 .ForMember(d => d.Daily, o => o.MapFrom(s => s.Data.daily.data));
+        }
+
+        private static DateTimeOffset FromUnixTimeToDateTime(long time)
+        {
+            return DateTimeOffset.FromUnixTimeSeconds(time);
         }
     }
 }
